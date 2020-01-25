313360bb19e0145d1bc5132836a7e25ef0b63077
Five Grant <5@fivegrant.com>
2020-01-25 11:55:06 -0500

    Demo version 1

--- init.sh
+++ init.sh
@@ -2,3 +2,9 @@
 
 # Load configuration file into express's root
 cp config.json api/control.json
+cd api
+npm install
+cd ../view
+npm install 
+
+
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+nodemon api/bin/www
+cd view
+npm start
+
